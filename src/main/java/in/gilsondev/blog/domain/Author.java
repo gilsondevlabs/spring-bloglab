9bafdec464dbebde36140a0e2593a87c37335533
Gilson da Silva Borges Filho <gilson.filho@basis.com.br>
2017-01-20 22:44:45 -0200

    Testando persistencia da entidade Author

--- src/main/java/in/gilsondev/blog/domain/Author.java
+++ src/main/java/in/gilsondev/blog/domain/Author.java
@@ -7,7 +7,10 @@ import javax.persistence.*;
 
 @Getter
 @Setter
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
--- src/test/java/in/gilsondev/blog/domain/AuthorEntityTest.java
+++ src/test/java/in/gilsondev/blog/domain/AuthorEntityTest.java
@@ -2,6 +2,9 @@ package in.gilsondev.blog.domain;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,7 +12,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class AuthorEntityTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
     public void shouldHaveLombokProperties() {
         Author author = new Author();
@@ -22,5 +29,20 @@ public class AuthorEntityTest {
         assertThat(author.getFirstName()).isEqualTo("Author");
         assertThat(author.getLastName()).isEqualTo("Test");
         assertThat(author.getEmail()).isEqualTo("author.test@mail.com");
+    }
+
+    @Test
+    public void shouldPersistData() {
+        Author author = new Author();
+        author.setFirstName("Author");
+        author.setLastName("Test");
+        author.setEmail("author.test@mail.com");
+
+        Author authorPersisted = entityManager.persistFlushFind(author);
+
+        assertThat(authorPersisted.getId()).isEqualTo(1L);
+        assertThat(authorPersisted.getFirstName()).isEqualTo(author.getFirstName());
+        assertThat(authorPersisted.getLastName()).isEqualTo(author.getLastName());
+        assertThat(authorPersisted.getEmail()).isEqualTo(author.getEmail());
     }
 }
