c144bb7b774a9a5ffca02df1034671a53debe550
Gilson da Silva Borges Filho <gilson.filho@basis.com.br>
2017-01-21 17:23:19 -0200

    Criando teste para validar persistencia do Post, e associado a um Autor

--- src/main/java/in/gilsondev/blog/domain/Post.java
+++ src/main/java/in/gilsondev/blog/domain/Post.java
@@ -3,13 +3,19 @@ package in.gilsondev.blog.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
     private Author author;
     private String title;
     private String slug;
@@ -18,5 +24,7 @@ public class Post {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private boolean status;
+
+    @ElementCollection
     private List<String> keywords;
 }
--- src/test/java/in/gilsondev/blog/domain/PostEntityTest.java
+++ src/test/java/in/gilsondev/blog/domain/PostEntityTest.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,6 +25,9 @@ import static org.powermock.api.mockito.PowerMockito.when;
 @SpringBootTest
 @DataJpaTest
 public class PostEntityTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
     public void shouldHaveLombokProperties() {
         Author authorFake = new AuthorBuilder().build();
@@ -54,5 +59,36 @@ public class PostEntityTest {
         assertThat(post.getUpdatedAt()).isEqualTo(LocalDateTime.now());
         assertThat(post.isStatus()).isEqualTo(true);
         assertThat(post.getKeywords()).containsAll(Arrays.asList("Post", "Tags"));
+    }
+
+    @Test
+    public void shouldPersistData() {
+        Author authorPersisted = entityManager.persistFlushFind(new AuthorBuilder().withId(null).build());
+
+        Post post = new Post();
+        post.setAuthor(authorPersisted);
+        post.setTitle("Post Title");
+        post.setSlug("post-title");
+        post.setTeaser("POst resume");
+        post.setBody("Post Content");
+        post.setCreatedAt(LocalDateTime.now());
+        post.setUpdatedAt(LocalDateTime.now());
+        post.setStatus(true);
+        post.setKeywords(Arrays.asList("Post", "Tags"));
+
+        Post postPersisted = entityManager.persistFlushFind(post);
+
+        assertThat(postPersisted.getId()).isNotNull();
+        assertThat(postPersisted.getId()).isNotNegative();
+        assertThat(postPersisted.getAuthor().getId()).isNotNull();
+        assertThat(postPersisted.getAuthor().getId()).isNotNegative();
+        assertThat(postPersisted.getTitle()).isEqualTo(post.getTitle());
+        assertThat(postPersisted.getSlug()).isEqualTo(post.getSlug());
+        assertThat(postPersisted.getTeaser()).isEqualTo(post.getTeaser());
+        assertThat(postPersisted.getBody()).isEqualTo(post.getBody());
+        assertThat(postPersisted.getCreatedAt()).isEqualTo(post.getCreatedAt());
+        assertThat(postPersisted.getUpdatedAt()).isEqualTo(post.getUpdatedAt());
+        assertThat(postPersisted.isStatus()).isEqualTo(post.isStatus());
+        assertThat(postPersisted.getKeywords()).containsAll(post.getKeywords());
     }
 }
